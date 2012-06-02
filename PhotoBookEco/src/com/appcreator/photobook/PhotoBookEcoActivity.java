42628fa3473173de44e47492043fb96d09ef7ea7
JSHwang <jshwang80@gmail.com>
2012-06-02 14:18:50 +0900

    init

---
+++ PhotoBookEco/src/com/appcreator/photobook/PhotoBookEcoActivity.java
@@ -0,0 +1,13 @@
+package com.appcreator.photobook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhotoBookEcoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
