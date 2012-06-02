139f17ebc7a6d3f4e5f03029eb5e298b903d1e19
choong <gyeong.mh@gmail.com>
2012-06-02 14:33:13 +0900

    commit test

--- PhotoBookEco/src/com/appcreator/photobook/PhotoBookEcoActivity.java
+++ PhotoBookEco/src/com/appcreator/photobook/PhotoBookEcoActivity.java
@@ -4,10 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class PhotoBookEcoActivity extends Activity {
-    /** Called when the activity is first created. */
+	
+	final int resIntroID 	= R.id.intro;
+	final int resLayoutID 	= R.layout.main;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(resLayoutID);
     }
+    
+    
 }
