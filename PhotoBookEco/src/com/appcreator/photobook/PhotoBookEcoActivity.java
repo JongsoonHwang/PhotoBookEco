0a24438ad0eed556966c56ab7a340f34f0c2a6ab
JSHwang <jshwang80@gmail.com>
2012-06-02 15:18:53 +0900

    와꾸 완료

--- PhotoBookEco/src/com/appcreator/photobook/PhotoBookEcoActivity.java
+++ PhotoBookEco/src/com/appcreator/photobook/PhotoBookEcoActivity.java
@@ -2,8 +2,13 @@ package com.appcreator.photobook;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class PhotoBookEcoActivity extends Activity {
+	
+	final String TAG = "PhotoBookEcoActivity";
 	
 	final int resIntroID 	= R.id.intro;
 	final int resLayoutID 	= R.layout.main;
@@ -12,6 +17,16 @@ public class PhotoBookEcoActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(resLayoutID);
+        
+       ((TextView)findViewById(resIntroID)).setOnClickListener(new View.OnClickListener() {
+
+    	   public void onClick(View v) {
+				
+    		   Log.e(TAG,"onClick");
+    		   
+    		   
+			}
+       });
     }
     
     
