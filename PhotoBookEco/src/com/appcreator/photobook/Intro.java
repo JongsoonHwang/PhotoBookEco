34b7c170a754d987477f1ccb5ae9f833d17f1edb
Sera Han <smart.hansera@gmail.com>
2012-06-11 16:25:42 +0900

    인트로 배경음

--- PhotoBookEco/src/com/appcreator/photobook/Intro.java
+++ PhotoBookEco/src/com/appcreator/photobook/Intro.java
@@ -1,17 +1,13 @@
 package com.appcreator.photobook;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 /*
  * 2012. 06. 02
@@ -27,6 +23,9 @@ public class Intro extends CommonActivity {
 	
 	ImageView ImgIntro;
 	static Handler mHandler;
+	SoundPool sp;
+	int introsound;
+
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,13 +33,24 @@ public class Intro extends CommonActivity {
 		
         setContentView(resLayoutID);
         
-//        ImgIntro  = (ImageView)findViewById(R.id.intro);
-//        Animation animation = null;
-//        animation = AnimationUtils.loadAnimation(Intro.this, R.anim.alpha);
-//        ImgIntro.startAnimation(animation);
+        sp = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        introsound = sp.load(this, R.raw.introsound, 1);
         
-        //mHandler = new Handler();
-        //mHandler.postDelayed(runnable, 6000);
+        ImgIntro  = (ImageView)findViewById(R.id.intro);
+        Animation animation = null;
+        animation = AnimationUtils.loadAnimation(Intro.this, R.anim.alpha);
+        ImgIntro.startAnimation(animation);
+        
+        mHandler = new Handler();
+        mHandler.postDelayed(runnable, 6000);
+        
+        
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+            	sp.play(introsound, 1, 1, 0, 0, 1);
+            }
+        }, 2000);
+        //sp.release();
     }
         
     Runnable runnable = new Runnable() {
@@ -48,7 +58,8 @@ public class Intro extends CommonActivity {
 		public void run() {
 			// TODO Auto-generated method stubs
 
-			Intent i = new Intent(Intro.this, NewPhotoBook.class);
+			Intent i = new Intent(Intro.this, PhotoBook.class);
+			//Intent i = new Intent(Intro.this, ThemeSlide.class);
 
 		    startActivity(i);
 		    finish();
